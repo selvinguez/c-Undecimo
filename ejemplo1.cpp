947076e02093ba05f6bf9a9d44a13decb3e51fdb
selvinguez <42685319+selvinguez@users.noreply.github.com>
2022-06-10 15:41:51 -0600

    ejemplo1

---
+++ ejemplo1.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main() {
+    cout << "Ingrese numero 1: ";
+    int numero1;
+    cin >> numero1;
+    cout << "Ingrese numero 2: ";
+    int numero2;
+    cin >> numero2;
+    cout << "La suma es: " << numero1+numero2 << endl;
+   cout << "La resta es: " << numero1-numero2 << endl;
+   cout << "La multi es: " << numero1*numero2 << endl;
+   cout << "La div es: " << numero1/numero2 << endl;
+    
+    return 0;
+  }
